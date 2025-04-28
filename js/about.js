7fbb841b20a7c0b18a7a253afc66af99293e410e
abdumudalib <abdumudalib1227@gmail.com>
2025-04-28 23:00:37 +0300

    add about page arrivals

--- js/about.js
+++ js/about.js
@@ -1,3 +1,39 @@
+const images = [
+  "/image/Brand Quality 100% Cotton Men T-shirt V-neck Fashion Design Slim Fit Soild T-shirts Male Tops Tees Short Sleeve T Shirt For Men v1 - F038-O-ArmyGreen _ CN Size S 50-55kg.jpg",
+  "/image/Women's & Men's Clothing, Shop Online Fashion.jpg",
+  "/image/Zeus - polohemd mit tigerdruck - Zebra _ 3XL.jpg",
+  "/image/Retro vintage denim shirt - Blue Gray _ M.jpg",
+  "/image/Women's Ivory Sneakers _ Womens Vergas Shoes _ Kizik.jpg",
+  "/image/Harber London _ Luxury leather goods_ Wallets, Covers & Bags.jpg",
+  "/image/29d3f6db-0b8e-4755-a175-6162735680ff.jpg",
+  "/image/Premium by Jack & Jones casual formel.jpg",
+  "/image/pngtree-digital-retailing-illustration-laptop-keyboard-with-shopping-basket-and-e-commerce-image_3903657.jpg",
+  "/image/Homme Tenant Une Veste Grise Sur Cintre, à La Recherche, En Choisissant_ _ Photo Gratuite.jpg"
+];
+
+
+// Function to switch images
+const imageElements = document.querySelectorAll('.image-item');
+let imageIndex = 0;
+
+// Function to set images
+function setImages() {
+  for (let i = 0; i < imageElements.length; i++) {
+    const img = images[(imageIndex + i) % images.length];
+    imageElements[i].style.backgroundImage = `url('${img}')`;
+  }
+  imageIndex = (imageIndex + 1) % images.length;
+}
+
+// Start showing images
+setImages();
+setInterval(setImages, 2000); // Change every 3 seconds
+
+
+
+
+
+
 const faqs = [
     {
       question: "What products does HalArag offer?",
